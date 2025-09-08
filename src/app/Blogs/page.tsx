d81ca13d1d69021bb5b3b96a542710e3ab47f77b
hshehriyaralam <hshehriyaralam@gmail.com>
2025-09-08 19:43:54 +0500

    add seacrh filter in Blogs page

--- src/app/Blogs/page.tsx
+++ src/app/Blogs/page.tsx
@@ -18,6 +18,18 @@ export default function AllBlogs() {
   const { data, isLoading } = useFetchBlogQuery([]);
   const { themeValue, light, dark, lightText, DarkText } = useContext(ContextTheme);
   const [showFilters, setShowFilters] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("")
+
+  // filter Blogs
+  const filteredBlogs = data?.data?.filter((blog: any) => {
+  const query = searchQuery.toLowerCase();
+  return (
+    blog.blogTitle.toLowerCase().includes(query) ||
+    blog.blogContent.toLowerCase().includes(query) ||
+    blog.userId?.name?.toLowerCase().includes(query)
+  );
+});
+
 
   return (
     <div className={`min-h-screen ${themeValue ? `${light}` : `${dark}`} py-8`}>
@@ -39,6 +51,8 @@ export default function AllBlogs() {
             themeValue={themeValue}
             light={light}
             dark={dark}
+            value={searchQuery}
+            onChange={(e: any) => setSearchQuery(e.target.value)}
             />
 
             {/* Filter Toggle */}
@@ -91,20 +105,20 @@ export default function AllBlogs() {
 
         {/* Blog Grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {data?.data?.map((blog: any, index: number) => {
-            const isFeatured = index % 6 === 0;
-            return (
-              <div key={blog._id} className={`border ${isFeatured ? 'lg:col-span-2' : ''} ${themeValue ? `${light}  border-gray-200 ` : `${dark} border-gray-700 `} rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-[1.02] cursor-pointer `}>
-                <BlogCard 
-                  blog={blog}
-                  isFeatured={isFeatured}
-                  themeValue={themeValue}
-                  lightText={lightText}
-                  DarkText={DarkText}
-                  isLoading={isLoading} />
-              </div>        
-            );
-          })}
+       {filteredBlogs?.map((blog: any, index: number) => {
+        const isFeatured = index % 6 === 0;
+        return (
+          <div key={blog._id} className={`border ${isFeatured ? 'lg:col-span-2' : ''} ${themeValue ? `${light}  border-gray-200 ` : `${dark} border-gray-700 `} rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-[1.02] cursor-pointer `}>
+            <BlogCard 
+              blog={blog}
+              isFeatured={isFeatured}
+              themeValue={themeValue}
+              lightText={lightText}
+              DarkText={DarkText}
+              isLoading={isLoading} />
+          </div>        
+        );
+      })}
         </div>
       </div>
     </div>
--- src/components/BlogsComponents/SearchINput.tsx
+++ src/components/BlogsComponents/SearchINput.tsx
@@ -3,13 +3,15 @@ import { Search } from "lucide-react";
 
 
 
-export default function SearchInput({themeValue, light, dark}:any){
+export default function SearchInput({themeValue, light, dark,value,onChange}:any){
     return(
          <div className="flex-1 relative">
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
         <input
         type="text"
         placeholder="Search blogs by title, content, or author..."
+        value={value}
+        onChange={onChange}
         className={`w-full pl-10 pr-4 py-3 rounded-lg border ${
             themeValue 
             ? `${light} border-gray-300 text-gray-800 placeholder-gray-800` 
